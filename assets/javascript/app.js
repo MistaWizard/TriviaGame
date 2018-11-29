72ae3901c5d6dbfb23cef046a6b394914b7465ad
MistaWizard <tylermckellar@gmail.com>
2018-11-29 14:12:17 -0500

    timer working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,38 @@
 $(document).ready(function() {
 
+
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unanswerCount = 0;
+    var timer = 20;
+    var intervalId;
+    var userGuess ="";
+    var running = false;
+    // var qCount = options.length;
+    // var pick;
+    // var index;
+    // var newArray = [];
+    // var holder = [];
+
+    //timer start
+function runTimer(){
+	if (!running) {
+	intervalId = setInterval(decrement, 1000); 
+	running = true;
+	}
+}
+//timer countdown
+function decrement() {
+	$("#pokeTime").html(timer);
+	timer --;
+
+	//stop timer if reach 0
+	if (timer === 0) {
+		unanswerCount++;
+		stop();
+		$("#questionSection").html("<p>Time is up!</p>");
+	}	
+}
+
+runTimer();
 })
